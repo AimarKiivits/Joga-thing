4bc004323420c802abe7190e8911864d9cc32e62
Aimar Kiivits <Aimarkiivits1@gmail.com>
2024-06-14 23:18:27 +0300

    Created author controller and route, refactored application code

--- index.js
+++ index.js
@@ -19,23 +19,11 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const articleRouter = require('./routes/article');
+const authorRouter = require('./routes/author');
 
 app.use('/', articleRouter);
 app.use('/article', articleRouter);
-
-app.get('/author/:id', (req, res) => {
-    let query = `SELECT au.id as author_id, au.name as author, ar.name, ar.published, ar.slug, ar.image, ar.body FROM article ar JOIN author au on ar.author_id = au.id WHERE au.id= "${req.params.id}"`;
-    let articles = [];
-    let author_name
-    con.query(query, function(err, result) {
-        if (err) throw err;
-        author_name = result[0].author;
-        console.log(author_name);
-        articles = result;
-        console.log(articles);
-        res.render('author', { articles: articles, author_name: author_name});
-    });
-});
+app.use('/author', authorRouter);
 
 app.listen(3000, () => {
     console.log('Server is running at port 3000');
